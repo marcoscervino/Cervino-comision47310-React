e16d55e48f62471db39633b0e8ad169c841a9112
marcos cervino <marcos.cervino@hotmail.com>
2023-07-22 16:50:27 -0300

    modificacion config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React47130-MarcosCervino/",
+  base: "/Cervino-comision47310-React/",
 })
