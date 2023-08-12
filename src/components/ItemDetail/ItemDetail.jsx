29d845c3e69b7db44563e1ab3669573c87399739
marcos cervino <marcos.cervino@hotmail.com>
2023-08-11 22:08:33 -0300

    funcionamiento correcto segunda entrega pre after class

--- src/components/ItemDetail/ItemDetail.jsx
+++
@@ -1,9 +0,0 @@
-export function ItemDetail ({nombre, precio, foto}) {
-    return <article>
-        <div><img src={foto} alt="" /></div>
-        <h5>{nombre}</h5>
-        <p>{precio}</p>
-    </article>
-}
-
-//1:27 link productdetail clase 9
